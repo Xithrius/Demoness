95a67eeb9d2c23437adb14656f98c22298c0348f
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-26 02:22:10 -0700

    Shoving everything into one file

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ import collections
 import json
 import os
 import aiohttp
+import traceback
 
 from discord.ext import commands as comms
 import discord
@@ -55,34 +56,45 @@ class Robot(comms.Bot):
                                  object_hook=lambda d: collections.namedtuple(
                                  "config", d.keys())(*d.values()))
 
-        #: Checking if database exists. If database does not exist, tables are created for the requesters
-        self.db_path = path('data', 'requests.db')
-        
-        if not os.path.isfile(self.db_path):
-            self.conn = sqlite3.connect(self.db_path)
-            self.cur = self.conn.cursor()
-            self.cur.execute('''CREATE TABLE IF NOT EXISTS Requests(
-                id INTEGER
-                title TEXT,
-                subreddit TEXT,
-                url TEXT,
-                upvotes INTEGER)''')
-            self.cur.execute('''CREATE TABLE IF NOT EXISTS Users(
-                id INTEGER,
-                request_number INTEGER,
-                subreddits TEXT,
-                warnings INTEGER)''')
-            self.conn.commit()
+        #: Checking if database exists.
+        self.check_database()
 
         #: Create async loop
         self.loop = asyncio.get_event_loop()
 
         future = asyncio.gather()
+        
+        #: Creating tasks
         self.loop.create_task(self.create_connections())
         self.loop.create_task(self.check_subreddits())
         self.loop.run_until_complete(future)
 
+        #: Adding checks
+
+        #: Adding commands
+        self.add_command(self.exit)
+
     """ subclass-specific tasks """
+
+    def check_database(self):
+        self.db_path = path('data', 'requests.db')
+
+        self.conn = sqlite3.connect(self.db_path)
+        self.cur = self.conn.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS Requests(
+            id INTEGER
+            title TEXT,
+            subreddit TEXT,
+            url TEXT,
+            upvotes INTEGER)''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS Users(
+            id INTEGER,
+            request_number INTEGER,
+            subreddits TEXT,
+            warnings INTEGER)''')
+        self.cur.execute('''INSERT INTO Users VALUES (?, ?, ?, ?)''',
+                         (196664644113268736, 0, 'pics', 0))
+        self.conn.commit()
 
     async def create_connections(self):
         """Session and database connections while testing service status.
@@ -95,7 +107,8 @@ class Robot(comms.Bot):
         cs.s('Client session created.')
 
     async def check_subreddits(self):
-        user_requests = self.cur.fetchall('''SELECT * FROM Users''')
+        user_requests = self.cur.execute('''SELECT * FROM Users''')
+        print(user_requests)
 
     """ Subclass-specific functions """
 
@@ -129,52 +142,6 @@ class Robot(comms.Bot):
                                    activity=discord.Game('With messages'))
         cs.r('Startup completed.')
 
-    async def close(self):
-        """ Safely closes connections
-
-        Returns & Raises:
-            Nothing since they're all passed.
-
-        """
-        try:
-            await self.session.close()
-            self.conn.close()
-        except Exception:
-            pass
-        await super().close()
-
-
-class MainCog(comms.Cog):
-    """."""
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    """ Checks """
-
-    async def cog_check(self, ctx):
-        """Checks if the command caller is an owner.
-
-        Returns:
-            True or false, on config.json's 'owner' contents.
-
-        """
-        return await self.bot.is_owner(ctx.author)
-
-    @comms.command(aliases=['disconnect', 'dc'])
-    async def exit(self, ctx):
-        """Logs out the bot.
-
-        Returns:
-            A possible timeout error.
-
-        """
-        cs.w('Logging out...')
-        await ctx.bot.logout()
-
-    """ Events """
-
-    @comms.Cog.listener()
     async def on_command_error(self, ctx, error):
         """Catches errors caused by users
 
@@ -208,9 +175,41 @@ class MainCog(comms.Cog):
             traceback.print_exception(
                 type(error), error, error.__traceback__, file=sys.stderr)
 
+    async def close(self):
+        """ Safely closes connections
+
+        Returns & Raises:
+            Nothing since they're all passed.
+
+        """
+        try:
+            await self.session.close()
+            self.conn.close()
+        except Exception:
+            pass
+        await super().close()
+
+    """ Commands """
+
+    @staticmethod
+    @comms.command(aliases=['disconnect', 'dc'])
+    @comms.is_owner()
+    async def exit(ctx):
+        """Logs out the bot.
+
+        Returns:
+            A possible timeout error.
+
+        """
+        cs.w('Logging out...')
+        try:
+            await ctx.message.delete()
+        except discord.errors.Forbidden:
+            pass
+        await ctx.bot.logout()
+
 
 if __name__ == "__main__":
     bot = Robot(command_prefix=comms.when_mentioned_or('.'),
                 case_insensitive=True)
-    bot.add_cog(MainCog(bot))
     bot.run(bot.config.discord, bot=True, reconnect=True)
